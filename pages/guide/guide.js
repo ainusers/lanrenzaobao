535f6f360d4227645e44fef386bc83364ab4cf78
tianyong <979111986@qq.com>
2020-12-23 14:42:06 +0800

    引导页引入

---
+++ pages/guide/guide.js
@@ -0,0 +1,20 @@
+//guide.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '懒人早读',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  // 引导页自动跳转
+  onShow: function(){
+    setTimeout(function(){
+      wx.navigateTo({
+        url: '../index/index',
+      })
+    },1500)
+  }
+})
