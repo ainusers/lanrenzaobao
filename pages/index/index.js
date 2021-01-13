719eedbc58301a6da7c485bebd31c3b4aca98577
tianyong <979111986@qq.com>
2021-01-13 14:54:31 +0800

    小程序下拉/上滑

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,75 @@
 var app = getApp();
 Page({
     data:{
-        winHeight:"",//窗口高度
+        winHeight:"100%",//窗口高度
         currentTab:0, //预设当前项的值
         scrollLeft:0, //tab标题的滚动条位置
-        initData: [],
-        triggered:true // 控制是否自定义刷新
+        initData: [], // 热点数据集
+        triggered:true, // 控制是否自定义刷新
+        isUpperPulled: false, // 判断是否为上拉操作
+        status: [1,1,1,1,1,1,1,1,1] // 管理当前模块下的page
+    },
+    getPageData(page) {
+        wx.request({
+            url: 'http://192.168.2.114:8888/service/page',
+            header: {'content-type': 'application/json;charset=UTF-8;'},
+            method: 'GET',
+            data: {page: page,type: this.data.currentTab}, 
+            complete: (res)=> {
+                const data = res.data
+                if(data.length>0){
+                    // 取出当前页默认的* (10)条数据
+                    for(let i=0,length=this.data.initData.length;i<length;i++){
+                        if(i == this.data.currentTab){
+                            this.data.initData[i] = this.data.initData[i].concat(res.data)
+                        }
+                    }
+                    this.setData({
+                        isUpperPulled: false,
+                        initData: this.data.initData
+                    })
+                }else{
+                    // 没有更多数据
+                    this.setData({
+                        isUpperPulled: false,
+                    })
+                }
+            }
+        })
+    },
+    scrollToLower: function(){
+        // 获取当前模块的page
+        let page = this.data.status[this.data.currentTab]
+        if(!this.data.isUpperPulled){
+            this.setData({
+                isUpperPulled: true,
+            })
+            // 更新当前页page
+            this.data.status[this.data.currentTab] = page + 1
+            console.log(this.data.status)
+            this.getPageData(this.data.status[this.data.currentTab])
+        }
     },
     // 下拉刷新
     refresh(){
-      // setTimeout(()=>{
-      //   this.setData({
-      //     triggered:false
-      //   })
-      // },3000)
-      wx.request({
-        url: 'http://192.168.2.114:8888/service/refresh',
-        data:{type:'maoyan'},
-        header: {'content-type': 'application/json'},
-        success: res => {
-            console.log(res.data)
-            this.setData({
-              triggered:false
-            })
-        }
-    })
+        wx.request({
+            url: 'http://192.168.2.114:8888/service/refresh',
+            data:{type:this.data.currentTab},
+            header: {'content-type': 'application/json;charset=UTF-8;'},
+            method: 'GET',
+            success: res => {
+                for(let i=0,length=this.data.initData.length;i<length;i++){
+                    if(i == this.data.currentTab){
+                        this.data.initData[i] = res.data
+                        this.data.status[this.data.currentTab] = 1
+                    }
+                }
+                this.setData({
+                    initData:this.data.initData,
+                    triggered:false,
+                })
+            }
+        })
     },
     // 滚动切换标签样式
     switchTab:function(e){
@@ -34,24 +79,9 @@ Page({
         });
         that.checkCor();
     },
-    // 点击或长按复制文本连接
-    // copyLink:function(){
-    //     var that = this;
-    //     var currentIndex = e.currentTarget.dataset.idx;
-    //     console.log(currentIndex); 
-    //     console.log(that.initData[currentIndex].link)
-    //     wx.setClipboardData({
-    //         data: that.initData[currentIndex].link,
-    //         success: function (res) {
-    //             wx.showToast({
-    //                 title: '复制成功',
-    //             });
-    //         }
-    //     });
-    // },
     // 点击标题切换当前页时改变样式
     swichNav:function(e){
-　　　　　var that = this;　
+　　　　var that = this;　
         var cur=e.target.dataset.current;
         if(that.data.currentTaB==cur){return false;}
         else{
@@ -81,19 +111,18 @@ Page({
                 var clientHeight=res.windowHeight,
                     clientWidth=res.windowWidth,
                     rpxR=750/clientWidth;
-              var  calc=clientHeight*rpxR-180;
-                console.log(calc)
-                that.setData( {  
-                    winHeight: calc  
-                });  
+                var calc=clientHeight*rpxR-180;
+                    that.setData( {  
+                        winHeight: calc  
+                    });  
             }  
         });
         // 初始化热点数据
         wx.request({
             url: 'http://192.168.2.114:8888/service/init',
-            header: {'content-type': 'application/json'},
+            header: {'content-type': 'application/json;charset=UTF-8;'},
+            method: 'GET',
             success: res => {
-                console.log(res.data)
                 this.setData({
                     initData: res.data
                 })
