6cf3fc601943d8f882f75040f8e749d38ce58152
tianyong <979111986@qq.com>
2021-01-20 18:50:06 +0800

    layout reset

--- pages/index/index.js
+++ pages/index/index.js
@@ -158,7 +158,6 @@ Page({
             header: {'content-type': 'application/json;charset=UTF-8;'},
             method: 'GET',
             success: res => {
-                console.log(res.data)
                 this.setData({
                     count: res.data
                 })
